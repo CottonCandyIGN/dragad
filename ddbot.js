63a70354121d02fae8fc7ee582bc4d3cf993c3cf
CottonCandy <39328777+CottonCandyIGN@users.noreply.github.com>
2020-03-27 16:56:12 -0400

    Update ddbot.js

--- ddbot.js
+++ ddbot.js
@@ -2,21 +2,27 @@ const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}ping`)) {
-	message.channel.send('Pong.');
-} else if (message.content.startsWith(`${prefix}beep`)) {
-	message.channel.send('Boop.');
-	else if (message.content === `${prefix}server`) {
-message.channel.send(`The server's name is: ${message.guild.name}`);
-	}
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	} else if (command === 'server') {
+		message.channel.send(`This server's name is: ${message.guild.name}`);
+	} else if (command === 'user-info') {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+}
+});	
 	
 
 client.login(token);
